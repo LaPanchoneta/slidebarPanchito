698831422dd81ece9c45eee5b1193ec1cbd5fd2d
La Panchoneta <168029728+LaPanchoneta@users.noreply.github.com>
2024-06-10 18:59:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,4 @@ toggleBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
+console.log("traduccion eliminacion de algunos elementos que no necesitariamos si lo queremos usar a futuro y cambios en el logo que lo cambiamos por un panchito")
